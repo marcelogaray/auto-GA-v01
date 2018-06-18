48c660fc8a0dffc0a91262c70af3008d699c0179
LinetTorrico <linet.torrico@dharbor.com>
2018-06-18 19:53:19 -0400

    Department List Test Case
    Department List Test Case

--- src/main/java/org/umssdiplo/automationv01/core/managepage/department/DepartmentList.java
+++ src/main/java/org/umssdiplo/automationv01/core/managepage/department/DepartmentList.java
@@ -1,14 +1,14 @@
 /**
  * @author: Miguel Rojas
  */
-package org.umssdiplo.automationv01.core.managepage.role;
+package org.umssdiplo.automationv01.core.managepage.department;
 
 import org.umssdiplo.automationv01.core.customwebdriver.ManageDriver;
 import org.umssdiplo.automationv01.core.managepage.BasePage;
 import org.umssdiplo.automationv01.core.utils.Constants;
 
-public class RoleList extends BasePage {
-    public static final String ROLE_LIST_PATH = Constants.SSIA_BASE_URL + "/roleList";
+public class DepartmentList extends BasePage {
+    public static final String ROLE_LIST_PATH = Constants.SSIA_BASE_URL + "/departmentList";
 
     public static void load() {
         ManageDriver.getInstance().getWebDriver()
--- src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/department/DepartmentListStepDefs.java
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/department/DepartmentListStepDefs.java
@@ -1,21 +1,22 @@
 /**
  * @author: Miguel Rojas
  */
-package org.umssdiplo.automationv01.stepdefinitionproject.role;
+package org.umssdiplo.automationv01.stepdefinitionproject.department;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import org.umssdiplo.automationv01.core.managepage.home.SSIAHome;
-import org.umssdiplo.automationv01.core.managepage.role.RoleList;
+import org.umssdiplo.automationv01.core.managepage.department.DepartmentList;
 
-public class RoleListStepDefs {
+public class DepartmentListStepDefs {
     @Given("^'SSI-A' home page is loaded$")
     public void ssiAHomePageIsLoaded() throws Throwable {
         SSIAHome.load();
     }
 
+
     @Then("^Load role list$")
     public void loadRoleList() throws Throwable {
-        RoleList.load();
+        DepartmentList.load();
     }
 }
