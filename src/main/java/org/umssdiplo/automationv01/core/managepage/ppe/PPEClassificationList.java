00165f944cf0d68eb97fc72b2aa09ab1320f0dab
wcolina <walker.cbdiv@gmail.com>
2018-06-25 23:18:00 -0400

    Addes cosntructor in page.

--- src/main/java/org/umssdiplo/automationv01/core/managepage/ppe/PPEClassificationList.java
+++ src/main/java/org/umssdiplo/automationv01/core/managepage/ppe/PPEClassificationList.java
@@ -3,15 +3,22 @@
  */
 package org.umssdiplo.automationv01.core.managepage.ppe;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.umssdiplo.automationv01.core.managepage.BasePage;
-import org.umssdiplo.automationv01.core.utils.CommonEvents;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.support.FindBy;
+        import org.umssdiplo.automationv01.core.managepage.BasePage;
+        import org.umssdiplo.automationv01.core.utils.CommonEvents;
 
 public class PPEClassificationList extends BasePage {
 
     @FindBy(id = "btnNewPpeClassification")
     private WebElement newPPEClassificationButton;
+
+    @FindBy (className = "container")
+    private WebElement ppeClassificationContainer;
+
+    public PPEClassificationList(){
+        CommonEvents.isPresent(ppeClassificationContainer);
+    }
 
     public boolean isPPEClassificationListPresent() {
         return CommonEvents.isPresent(newPPEClassificationButton);
